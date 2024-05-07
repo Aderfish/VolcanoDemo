aee076e94c2e95353150afa1d18efa1872ccae30
Alberts Reisons <alberts.reisons@epfl.ch>
2024-05-07 16:55:57 +0200

    fix(particles): pass camera state correctly

--- volcano/src/particles/fire_actors.js
+++ volcano/src/particles/fire_actors.js
@@ -32,7 +32,7 @@ export function init_billboard_actor(
         uniforms: {
             mat_mvp: regl.prop("mat_mvp"),
             camera_right_world: regl.prop("camera_right_world"),
-            camera_up_world: regl.prop("camera_right_world"),
+            camera_up_world: regl.prop("camera_up_world"),
             billboard_size: regl.prop("billboard_size"),
             billboard_center_worldspace: regl.prop("billboard_center_worldspace"),
         },
@@ -48,8 +48,8 @@ export function init_billboard_actor(
             this.mat_model_view = mat4.create();
             this.mat_normals = mat3.create();
             this.mat_model_to_world = mat4.create();
-            this.camera_right_world = vec3.fromValues(1., 0., 0.);
-            this.camera_up_world =   vec3.fromValues(0., 1., 0.);
+            this.camera_right_world = vec3.create();
+            this.camera_up_world =   vec3.create();
         }
 
         draw({ mat_projection, mat_view, light_position_cam }) {
@@ -63,15 +63,17 @@ export function init_billboard_actor(
             //this.camera_right_world = [this.mat_model_view[0][0], this.mat_model_view[1][0], this.mat_model_view[2][0]];
             //this.camera_up_world    = [this.mat_model_view[0][1], this.mat_model_view[1][1], this.mat_model_view[2][1]];
         
+            vec3.set(this.camera_right_world, 1., 0., 0.);
+            vec3.set(this.camera_up_world, 0., 1., 0.);
 
             pipeline_draw_billboard({
                 mat_mvp: this.mat_mvp,
                 
                 camera_right_world: this.camera_right_world,
-                camera_right_world: this.camera_right_world,
+                camera_up_world: this.camera_up_world,
 
-                billboard_size: vec2.fromValues(100., 300.),
-                billboard_center_worldspace: vec3.fromValues(0., 0., 0.),
+                billboard_size: vec2.fromValues(1., 3.),
+                billboard_center_worldspace: vec3.fromValues(0., 0., 1.),
             });
         }
     }
