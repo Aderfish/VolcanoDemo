9fbe6d0e0d8281582d52078b3b38180ae2414284
Yoann Lafore <yoann.lafore@gmail.com>
2024-05-10 20:50:19 +0200

    fix(lava): remove terrain elevation offset

--- volcano/src/terrain/shaders/terrain.frag.glsl
+++ volcano/src/terrain/shaders/terrain.frag.glsl
@@ -12,10 +12,10 @@ varying vec3 v2f_normal;
 
 const vec3  light_color = vec3(1.0, 0.941, 0.898) * 1.0;
 // Small perturbation to prevent "z-fighting" on the water on some machines...
-const float terrain_water_level    = -0.03125 + 1e-6;
+const float terrain_water_level    = 1e-6;
 const vec3  terrain_color_water    = vec3(0.29, 0.51, 0.62);
 const vec3  terrain_color_mountain = vec3(0.8, 0.5, 0.4);
-const vec3  terrain_color_grass    = vec3(0.33, 0.43, 0.18);
+const vec3  terrain_color_grass    = vec3(0.36);
 
 void main()
 {
